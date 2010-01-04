197f2191d332d3abda7f8dbac202e42fd00c9ce6
Ben Blackburne <bpb@lysozyme.net>
2010-01-04 12:45:43 +0000

    Added a project file to get required jars

---
+++ project/build/src/ModiphyProject.scala
@@ -0,0 +1,13 @@
+import sbt._
+
+class ModiphyProject(info: ProjectInfo) extends DefaultProject(info)
+{
+  override def compileOptions = super.compileOptions ++ Seq(Unchecked)
+
+  val logspace = "Logspace maven repo" at "http://www.logspace.co.uk/maven/"
+  val tlf = "uk.co.logspace.tlf" % "tlf" % "1.0.1"
+  val colt = "coltjar" % "colt" % "1.2.0" from "http://repo1.maven.org/maven2/colt/colt/1.2.0/colt-1.2.0.jar"
+  val pal = "pal" % "pal" % "1.5.1" from "http://www.cebl.auckland.ac.nz/pal-project/bin/pal-1.5.1.jar"
+  val commonsMath = "org.apache.commons" % "commons-math" % "2.0"
+  
+}
