bdde6e93d0d725a4283983a2665139fc12bdf6ec
Ben Blackburne <bpb@lysozyme.net>
2010-02-17 15:34:05 +0000

    SBT 0.7.0 compliant

--- project/build/src/ModiphyProject.scala
+++ project/build/src/ModiphyProject.scala
@@ -11,6 +11,6 @@ class ModiphyProject(info: ProjectInfo) extends DefaultProject(info)
   val commonsMath = "org.apache.commons" % "commons-math" % "2.0"
   val commonsCollections = "commons-collections" % "commons-collections" % "3.2.1"
   val beastBeauti = "dr.math" % "beauti" % "1.5.3" from "http://www.logspace.co.uk/jar/beauti-1.5.3.jar"
-  val sbt = "sbt" % "sbt_2.7.7" % "0.5.6" from "http://simple-build-tool.googlecode.com/svn-history/r1116/artifacts/0.5.6/jars/sbt_2.7.7.jar"
+  val sbt = "sbt" % "sbt_2.7.7" % "0.5.7" from "http://simple-build-tool.googlecode.com/svn-history/r1125/artifacts/0.5.7-p1/jars/sbt_2.7.7.jar"
   
 }
