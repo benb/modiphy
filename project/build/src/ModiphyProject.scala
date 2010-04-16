97c7b940feddaf68f1f4f763b1cd8e991ffb7236
Ben Blackburne <bpb@lysozyme.net>
2010-04-16 11:56:07 +0100

    Bump depencency versions
    Scalatest unit tests don't run at the moment unformtunately (NB this is not
    the same as them failing, they won't run with RC1).

--- project/build/src/ModiphyProject.scala
+++ project/build/src/ModiphyProject.scala
@@ -5,12 +5,10 @@ class ModiphyProject(info: ProjectInfo) extends DefaultProject(info)
   override def compileOptions = super.compileOptions ++ Seq(Unchecked)
 
   val newReleaseToolsRepository = "Scala Tools Repository" at "http://nexus.scala-tools.org/content/repositories/snapshots/" 
-  val scalatest = "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.Beta1-with-test-interfaces-0.3-SNAPSHOT" % "test" 
-
-
+  val scalatest = "org.scalatest" % "scalatest" % "1.0.1-for-scala-2.8.0.RC1-SNAPSHOT" // % "test" 
 
   val logspace = "Logspace maven repo" at "http://www.logspace.co.uk/maven/"
-  val tlf = "uk.co.logspace.tlf" % "tlf_2.8.0.Beta1"  % "1.0.1"
+  val tlf = "uk.co.logspace.tlf" % "tlf_2.8.0.RC1"  % "1.2.0"
   val colt = "coltjar" % "colt" % "1.2.0" from "http://repo1.maven.org/maven2/colt/colt/1.2.0/colt-1.2.0.jar"
   val commonsMath = "org.apache.commons" % "commons-math" % "2.0"
   val commonsCollections = "commons-collections" % "commons-collections" % "3.2.1"
